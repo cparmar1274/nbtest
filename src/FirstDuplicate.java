a1687f2f63033d09af194797c8c34e7b48b4baaa
Chirag Parmar <chirag-parmar@live.com>
2021-03-08 12:52:53 -0500

    formatting

--- src/FirstDuplicate.java
+++ src/FirstDuplicate.java
@@ -5,15 +5,22 @@ import java.util.List;
 
 import org.junit.Test;
 
+/*
+ * 
+1) Write an efficient algorithm to find the first duplicated integer in an array of integers
+Input : arrrayA = {8, 2, 3, 0, 3, 6, 2}
+Output : 3
+Input : arrrayA = {5, 2, 4, 6, 3, 7, 1}
+Output : null
+Input : arrrayA = {10, 5, 0, 0, 5, 10}
+Output : 0
+ * */
+
 public class FirstDuplicate {
 
-	public static void main(String[] args) {
-		int[] arr = new int[] { 1, 5, 9, 8, 4, 4 };
-		System.out.println(findFirstDuplicate(arr));
-
-	}
-
 	public static String findFirstDuplicate(int[] arr) {
+		if (arr == null || arr.length <= 0)
+			return null;
 		List<Integer> temp = new ArrayList<>();
 		for (int i : arr) {
 			if (temp.indexOf(i) != -1)
@@ -24,13 +31,20 @@ public class FirstDuplicate {
 		return null;
 	}
 
+	public static void main(String[] args) {
+		int[] arr = new int[] { 1, 5, 9, 8, 4, 4 };
+		System.out.println(findFirstDuplicate(arr));
+
+	}
+
 	@Test
 	public void testFindFirstDuplicate() {
 		assertEquals(findFirstDuplicate(new int[] {}), null);
+		assertEquals(findFirstDuplicate(null), null);
 		assertEquals(findFirstDuplicate(new int[] { 1, 5, 9, 8, 4, 4 }), "4");
-		assertEquals(findFirstDuplicate(new int[] { 8,2,3,0,3,6,2 }), "3");
-		assertEquals(findFirstDuplicate(new int[] { 5,2,4,6,3,7,1 }), null);
-		assertEquals(findFirstDuplicate(new int[] { 10,5,0,0,5,10 }), "0");
+		assertEquals(findFirstDuplicate(new int[] { 8, 2, 3, 0, 3, 6, 2 }), "3");
+		assertEquals(findFirstDuplicate(new int[] { 5, 2, 4, 6, 3, 7, 1 }), null);
+		assertEquals(findFirstDuplicate(new int[] { 10, 5, 0, 0, 5, 10 }), "0");
 	}
 
 }
--- src/PrintNFollows.java
+++ src/PrintNFollows.java
@@ -2,11 +2,13 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class PrintNFollows {
+/*
+ * Write a method that takes in an integer N as input and prints N a’s followed by N b’s.
+i.e. if N=5, print aaaaabbbbb
+Note - Try writing the solution without using any loop structures
+ * */
 
-	public static void main(String[] args) {
-		System.out.println(printNFollow(5, 'a', "") + printNFollow(5, 'b', ""));
-	}
+public class PrintNFollows {
 
 	private static String printNFollow(int n, char c, String str) {
 		if (n <= 0)
@@ -15,12 +17,16 @@ public class PrintNFollows {
 		return printNFollow(--n, c, str);
 	}
 
+	public static void main(String[] args) {
+		System.out.println(printNFollow(5, 'a', "") + printNFollow(5, 'b', ""));
+	}
+
 	@Test
 	public void testPrintNFollows() {
-		assertEquals(printNFollow(5, 'a', "")+printNFollow(5, 'b', ""), "aaaaabbbbb");
+		assertEquals(printNFollow(5, 'a', "") + printNFollow(5, 'b', ""), "aaaaabbbbb");
 		assertEquals(printNFollow(2, 'a', ""), "aa");
 		assertEquals(printNFollow(1, 'a', ""), "a");
 		assertEquals(printNFollow(-1, 'a', ""), "");
-		
+
 	}
 }
